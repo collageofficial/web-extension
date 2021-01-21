24d0759daca80903df6884774a74b4006df3b2e2
JacopoLuri <geomjacopoluri@gmail.com>
2021-01-21 18:27:36 +0100

    button atoms done

--- extension/src/App.js
+++ extension/src/App.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Context } from './Context/Context'
+import './App.css'
 
 import Button from './Components/Atoms/Button'
 import Text from './Components/Atoms/Text'
@@ -8,24 +9,68 @@ const App = () => {
     const context = useContext(Context)
     return (
         <>
-            {/* test stuffs, delete them after */}
-            {/* <h1>hello</h1>
-      <h1>Counter with Hook State</h1>
-      <h1>{context.counter}</h1>
-      <button onClick={() => context.setCounter(context.counter + 1)}>
-        Increment
-      </button>
-      <hr /> */}
+            {/* test stuffs, delete them after
+            <h1>hello</h1>
+            <h1>Counter with Hook State</h1>
+            <h1>{context.counter}</h1>
+            <button onClick={() => context.setCounter(context.counter + 1)}>
+                Increment
+            </button>
+            <hr /> */}
             <Button
-                text="+"
+                text="Button"
+                color="light"
                 textSize="medium"
+                textWeight="normal"
                 bgColor="primary"
+                width="20"
+                height="10"
+                borderRadius="small"
+            />
+            <Button
+                text="Button"
+                color="dark"
+                textSize="medium"
+                textWeight="normal"
+                bgColor="light"
+                width="20"
+                height="10"
+                border="2"
+                borderColor="dark"
+                borderRadius="small"
+            />
+            <Button
+                text="Button"
+                color="primary"
+                textSize="medium"
+                textWeight="normal"
+                bgColor="light"
+                width="20"
+                height="10"
+                border="2"
+                borderColor="primary"
+                borderRadius="small"
+            />
+            <Button
+                special="plus-btn"
                 width="5"
                 height="5"
-                color="light"
                 borderRadius="full"
             />
-            {/* <Text text="Hello world!" bgColor="primary" color="light" /> */}
+            <Button
+                special="plus-btn-border"
+                width="5"
+                height="5"
+                border="2"
+                borderRadius="full"
+            />
+            <Button
+                special="plus-btn"
+                width="8"
+                height="8"
+                borderRadius="full"
+            />
+            {/* <Text text='Hello world!' bgColor='primary' color='light' /> */}
         </>
     )
 }
--- extension/src/Components/Atoms/Button.js
+++ extension/src/Components/Atoms/Button.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+// special classeName defined in App.css
+
 const Button = ({
     action,
+    special,
     text,
     textSize,
+    textWeight,
     bgColor,
     color,
     width,
@@ -15,60 +19,95 @@ const Button = ({
     <button
         onClick={action}
         className={`
-      bg-${bgColor} 
-      text-${color}
-      text-${textSize} 
-      w-${width} 
-      h-${height} 
-      border-${border} 
-      border-${borderColor} 
-      rounded-${borderRadius}
-      hover:bg-${color}
-      hover:text-${bgColor}
-      transition duration-500
-      focus:outline-none
-      active:outline-none
-    `}
+          ${special}
+          font-main
+          font-${textWeight}
+          text-center
+          align-middle
+          bg-${bgColor} 
+          text-${color}
+          text-${textSize} 
+          w-${width} 
+          h-${height} 
+          border-${border} 
+          border-${borderColor} 
+          rounded-${borderRadius}
+          hover:bg-${color}
+          hover:text-${bgColor}
+          transition duration-500
+          focus:outline-none
+          active:outline-none
+        `}
     >
         {text}
     </button>
 )
 
 export default Button
-
+// Basic button width 80px, height 40px translated in tailwind "numbers"
 // Basic primary color button
-// <Button
-//   text="Button"
-//   color="light"
-//   textSize="medium"
-//   bgColor="primary"
-//   width="20"
-//   height="10"
-//   borderRadius="small"
-// />
+{
+    /* <Button
+    text="Button"
+    color="light"
+    textSize="medium"
+    textWeight="normal"
+    bgColor="primary"
+    width="20"
+    height="10"
+    borderRadius="small"
+/> */
+}
 
 // Basic light color button
-// <Button
-//   text="Button"
-//   color="dark"
-//   textSize = 'medium'
-//   bgColor="light"
-//   width="20"
-//   height="10"
-//   border="2"
-//   borderColor="dark"
-//   borderRadius="small"
-// />
+{
+    /* <Button
+    text="Button"
+    color="dark"
+    textSize="medium"
+    textWeight="normal"
+    bgColor="light"
+    width="20"
+    height="10"
+    border="2"
+    borderColor="dark"
+    borderRadius="small"
+/> */
+}
 
 // Basic inverted primary color button
-// <Button
-//   text="Button"
-//   bgColor="light"
-//   color="primary"
-//   textSize = 'medium'
-//   width="20"
-//   height="10"
-//   border="2"
-//   borderColor="primary"
-//   borderRadius="small"
-// />
+{
+    /* <Button
+    text="Button"
+    color="primary"
+    textSize="medium"
+    textWeight="normal"
+    bgColor="light"
+    width="20"
+    height="10"
+    border="2"
+    borderColor="primary"
+    borderRadius="small"
+/> */
+}
+
+//  Plus sign no border 20px
+{
+    /* <Button special="plus-btn" width="5" height="5" borderRadius="full" /> */
+}
+
+// Plus sign border 20 px
+{
+    /* <Button
+    special="plus-btn-border"
+    width="5"
+    height="5"
+    border="2"
+    borderRadius="full"
+/> */
+}
+
+// Plus sign no border 32px
+{
+    /* <Button special="plus-btn" width="8" height="8" borderRadius="full" /> */
+}
