ffb5ff8a8ee2e10bf95a93c210ba5124739a4341
Gabriellji <gabriellji@yahoo.com>
2021-01-21 18:27:39 +0300

    fix: bugs

--- extension/tailwind.config.js
+++ extension/tailwind.config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 
   darkMode: false, // or 'media' or 'class'
 
   theme: {
     extend: {
       colors: {
-        primary: "#E60023",
-        light: "#ffffff",
-        dark: "#000000",
-        grey: "#DDDDDD",
+        primary: '#E60023',
+        light: '#ffffff',
+        dark: '#000000',
+        grey: '#DDDDDD',
       },
       fontFamily: {
-        main: ["Poppins"],
+        main: ['Poppins'],
       },
       fontSize: {
-        small: ["12"],
-        medium: ["18"],
-        large: ["24"],
+        small: ['12'],
+        medium: ['18'],
+        large: ['24'],
       },
       outline: {
-        none: "none",
+        none: 'none',
       },
     },
   },
@@ -29,4 +29,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+}
