79c005157027cc567dbdb606252b875f4c2371db
mancia08 <teikomancini1993@gmail.com>
2021-01-24 18:36:15 +0100

    contact form working

--- extension/src/Components/Organisms/CaptionForm.js
+++ extension/src/Components/Organisms/CaptionForm.js
@@ -1,19 +1,45 @@
-import React, { useContext } from 'react'
+import { findAllByTitle } from '@testing-library/react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Context } from './../../Context/Context'
 
 const CaptionForm = () => {
     const context = useContext(Context)
-
+    const [image, setImage] = useState(context.picturesToSave)
+    const [reload, setReload] = useState(false)
+    useEffect(() => {
+        setReload(false)
+        console.log('hi')
+        console.log(context.picturesToSave)
+    }, [reload])
     return (
-        <form onSubmit={handleSubmit}>
-            <img src=""/>
+        <form
+            onChange={(e) => {
+                e.preventDefault()
+                e.target.id === 'filename'
+                    ? (image[0].filename = e.target.value)
+                    : (image[0].caption = e.target.value)
+                context.setPicturesToSave(image)
+                setReload(true)
+            }}
+        >
+            <img src={image[0].src} />
             <div>
                 <label htmlFor="filename">Title:</label>
-                <input type="email" id="filename" />
+                <input
+                    type="text"
+                    id="filename"
+                    value={image[0].filename}
+                    required
+                />
             </div>
             <div>
                 <label htmlFor="caption">Caption:</label>
-                <input type="text" id="caption" />
+                <input
+                    type="text"
+                    id="caption"
+                    value={image[0].caption}
+                    required
+                />
             </div>
         </form>
     )
