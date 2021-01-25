1d280253186c86ad7e87bfb410f153c3e807b062
Gabriellji <gabriellji@yahoo.com>
2021-01-25 17:34:17 +0300

    feat: implement register for user

--- server/src/resources/user/users.js
+++ server/src/resources/user/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require('express-validator')
 
 const User = require('./User')
@@ -27,10 +28,20 @@ router.post(
             let user = await User.findOne({ email })
 
             if(user) {
-                res.status(400).json({ errors: [ { msg: 'User already exists' }]})
+                return res.status(400).json({ errors: [ { msg: 'User already exists' }]})
             }
 
-            res.send('User route')
+            user = new User({
+                name, email, password
+            })
+
+            const salt = await bcrypt.genSalt(10)
+
+            user.password = await bcrypt.hash(password, salt)
+
+            await user.save()
+
+            res.send('User registered')
 
         } catch (err) {
             console.error(err.message)
