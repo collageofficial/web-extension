e66d119811743d624dec8ecf9556183b9c76a3e7
mancia08 <teikomancini1993@gmail.com>
2021-01-26 15:55:25 +0100

    modal

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,1 +1,5 @@
-/* console.log("background.js") */
+chrome.browserAction.onClicked.addListener(function (tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { type: 'popup-modal' })
+    })
+})
--- extension/public/content.js
+++ extension/public/content.js
@@ -1,10 +1,46 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    request.message === 'give me pictures, please' && getData()
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.type === 'popup-modal') {
+        showModal()
+        getData()
+    }
 })
 
+const showModal = () => {
+    const modal = document.createElement('dialog')
+    modal.setAttribute(
+        'style',
+        `
+    height:80vh;
+    width:80vw;
+    position: absolute;
+    top: 50%;
+    margin-top: -40vh;
+    left:50%;
+    margin-left: -40vw;
+    border: none;
+    border-radius:20px;
+    background-color:white;
+     box-shadow: 0px 12px 48px rgba(29, 5, 64, 0.7);
+    `
+    )
+    modal.innerHTML = `<iframe id="popup-content"; style="height:100%; width:100%"></iframe>
+    <div style="position:absolute; top:0px; left:5px;">
+    <button style="padding: 8px 12px; font-size: 16px; border: none; border-radius: 20px;">x</button>
+    </div>`
+    document.body.appendChild(modal)
+    const dialog = document.querySelector('dialog')
+    dialog.showModal()
+    const iframe = document.getElementById('popup-content')
+    iframe.src = chrome.extension.getURL('index.html')
+    iframe.frameBorder = 0
+    dialog.querySelector('button').addEventListener('click', () => {
+        dialog.close()
+    })
+}
+
 const getData = () => {
-    let imagesToSend = []
     let imagesOnBrowser = []
+    let imagesToSend = []
 
     const getImages = async () => {
         imagesOnBrowser = Array.from(document.querySelectorAll('img'))
@@ -28,10 +64,22 @@ const getData = () => {
         )
     }
     const sendImages = async () => {
-        chrome.runtime.sendMessage({
-            from: 'content',
-            message: JSON.stringify(imagesToSend),
-        })
+        imagesToSend.map((image, index) =>
+            window.localStorage.setItem(
+                `${index}`,
+                JSON.stringify({
+                    caption: image.caption,
+                    filename: image.filename,
+                    origin: image.origin,
+                    size: {
+                        width: image.size.width,
+                        height: image.size.height,
+                    },
+                    ratio: image.ratio,
+                    src: image.src,
+                })
+            )
+        )
     }
 
     getImages().then(sortImages().then(sendImages()))
