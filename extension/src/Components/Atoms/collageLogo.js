b9c986cbce5f06e5a027e0ee1cc3df78dce2ff32
Kolko Valentin <valiant9191@gmail.com>
2021-01-27 00:12:28 +0300

    create atom logo

---
+++ extension/src/Components/Atoms/collageLogo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import collageLogo from './collage-logo.svg'
+
+const CollageLogo = () => {
+    return(
+    <div>
+<img src={collageLogo} className="App-logo" alt="logo" />
+</div>
+    
+    )}
+
+
+export default CollageLogo
