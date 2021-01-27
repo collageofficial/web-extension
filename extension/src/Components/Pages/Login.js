6143111483c720d194aeafff6978c7e293c83f75
mancia08 <teikomancini1993@gmail.com>
2021-01-27 13:55:26 +0100

    form created

--- extension/src/Components/Pages/Login.js
+++ extension/src/Components/Pages/Login.js
@@ -1,15 +1,51 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Context } from './../../Context/Context'
 
 const Login = () => {
     const context = useContext(Context)
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [reload, setReload] = useState(false)
 
+    const logIn = async (e) => {
+        e.preventDefault()
+        fetch('http://localhost:4000/auth', {
+            method: 'POST',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        }).then((res) => {
+            console.log(res)
+        })
+    }
+    const handleChange = (e) => {
+        e.preventDefault()
+        e.target.id === 'email'
+            ? setEmail(e.target.value)
+            : setPassword(e.target.value)
+        setReload(true)
+    }
+
+    useEffect(() => {
+        setReload(false), [reload]
+    })
     return (
         <>
-            hello. log in first please
-            <button onClick={context.exitLoginPage}>log in</button>
+            <form onChange={handleChange}>
+                <label htmlFor="email">Email:</label>
+                <input type="text" id="email" required />
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" required />
+                <button onClick={logIn}>log in</button>
+            </form>
         </>
     )
 }
 
 export default Login
+
+/* {context.exitLoginPage} */
